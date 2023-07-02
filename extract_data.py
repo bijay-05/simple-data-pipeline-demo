cd7dbdd9130d6b3ed7cef7217375d4df858b42ac
bijay-05 <hawkeyesniper69@gmail.com>
2023-07-02 21:03:41 +0545

    updated extract_data.py
    modified dict object

--- extract_data.py
+++ extract_data.py
@@ -13,12 +13,12 @@ def config(filename='database.ini', section='postgresql'):
     parser.read(filename)
 
     # get section, default to postgresql
-    db = {}
+    db_keys = {}
     if parser.has_section(section):
         params = parser.items(section)
         for param in params:
-            db[param[0]] = param[1]
+            db_keys[param[0]] = param[1]
     else:
         raise Exception('Section {0} not found in the {1} file'.format(section, filename))
 
-    return db
+    return db_keys
