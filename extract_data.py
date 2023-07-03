a31218054f5cdac0981ff91739f6067a301577e2
bijay-05 <hawkeyesniper69@gmail.com>
2023-07-03 21:32:25 +0545

    updated extract_data.py
    added function get_data

--- extract_data.py
+++ extract_data.py
@@ -1,5 +1,5 @@
 from configparser import ConfigParser
-
+import polars as pl
 
 def get_keys(filename='database.ini', section='postgresql'):
     """
@@ -29,4 +29,25 @@ def get_connection(keys : dict) -> str :
     for making connection to a database and returns a
     connection string (for polars.read_database function)
     """
-    return "postgres://{keys[user]}:{keys[password]}@{keys[host]}:{keys[port]}/{keys[database]}"
+    return f"postgres://{keys[user]}:{keys[password]}@{keys[host]}:{keys[port]}/{keys[database]}"
+
+def get_data(conn_string: str, table_name: str, output_format: int):
+    """
+    This function takes connection_string, table_name and
+    output_format as inputs and outputs the success of the function
+    output_format is integer value indicating 0: CSV and 1: PARQUET
+    """
+    query = "SELECT * FROM public"+table_name
+    
+    # read the data from database into dataframe
+    dataframe = pl.read_database(connection_uri=conn_string, query=query)
+
+    # output the data from database into file
+    if output_format == 0:
+        dataframe.write_csv(file=f"{table_name}_file.csv", has_header=True)
+        return "DONE !! Data written to file CSV"
+    elif output_format == 1:
+        dataframe.write_parquet(f"{table_name}_file.parquet")
+        return "DONE !! Data written to file PARQUET"
+    else:
+        print("Choose one of the output formats available")
