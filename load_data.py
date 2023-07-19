f3d4a74a05daa997cbd676176c1944b95ab7b70a
bijay-05 <hawkeyesniper69@gmail.com>
2023-07-19 21:57:24 +0545

    added loading into database

--- load_data.py
+++ load_data.py
@@ -1,5 +1,6 @@
-from snowflake.snowpark import Session
-import polars as pl
+import snowflake.connector as snow
+from snowflake.connector.pandas_tools import write_pandas
+import pandas as pd
 
 
 def get_connection(connection_keys: dict):
@@ -7,18 +8,44 @@ def get_connection(connection_keys: dict):
     This function takes dictionary of keys as input 
     and returns a connection object to snowflake datawarehouse
     """
+    account = connection_keys["account"]
+    user = connection_keys["user"]
+    password = connection_keys["password"]
+    warehouse = connection_keys["warehouse"]
+    database = connection_keys["database"]
+    table = connection_keys["table"]
 
     # Establish the connection
-    new_session = Session.builder.configs(connection_parameters).create()  
+    connection = snow.connect(
+        user=user,
+        password=password,
+        account=account,
+        database=database
+    )
 
-    return new_session
+    return connection
 
-def load_data(session: Session):
+def load_data(connection: snow.connect(), datetime:str):
     """
-    This function takes session object  as 
+    This function takes connection object  as 
     input and performs the loading of data into warehouse,
      finally returns the status of function completion
     """
-    session.create(df)
+
+    #read data from CSV (extract_data.py)
+    df = pd.read_csv("./sales_table_{datetime}_file.csv",delimiter=',', header=0) # header=0 means first line as columns
+    
+    # get the cursor
+    cursor = connection.cursor()
+    try:
+        cursor.execute(f"create table IF NOT EXISTS sales_table_{datetime}")
+        write_pandas(connection,df,f"sales_table_{datetime}")
+        result = cursor.execute(f"select count(*) from sales_table_{datetime}").fetchone()
+        print("Number of rows in table: ",result)
+    finally:
+        connection.close()
+    
     return "Data Loading Status: Completed"
+
+    
 
