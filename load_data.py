75865c3836b08ad12c7f2963430ea7353eab18ff
bijay-05 <hawkeyesniper69@gmail.com>
2023-07-28 12:33:07 +0545

    corrected write_pandas func args

--- load_data.py
+++ load_data.py
@@ -11,35 +11,46 @@ def get_connection(connection_keys: dict):
     account = connection_keys["account"]
     user = connection_keys["user"]
     password = connection_keys["password"]
+    warehouse = connection_keys["warehouse"]
     database = connection_keys["database"]
+    schema = connection_keys["schema"]
 
     # Establish the connection
-    connection = snow.connect(
-        user=user,
-        password=password,
-        account=account,
-        database=database
-    )
+    try:
+        connection_obj = snow.connect(
+            user=user,
+            password=password,
+            account=account,
+            warehouse=warehouse,
+            database=database,
+            schema=schema,
+            client_session_keep_alive=True
+        )
+    except:
+        raise Exception("connection object does not exist")
 
-    return connection
+    return connection_obj
 
-def load_data(connection: snow.connect(), last_date:str) -> str:
+def load_data(connection, last_date:str) -> str:
     """
-    This function takes connection object  as 
-    input and performs the loading of data into warehouse,
+    This function takes connection object and last_date (as date when
+    the pipeline last ran) as input and performs the loading of data into target database,
      finally returns the status of function completion
     """
 
     #read data from CSV (extract_data.py)
-    df = pd.read_csv("./sales_table_after_{last_date}_file.csv",delimiter=',', header=0) # header=0 means first line as columns
+    df = pd.read_csv(f"./sales_table_after_{last_date}_file.csv",delimiter=',', header=0) # header=0 means first line as columns
     
     # get the cursor
     cursor = connection.cursor()
     try:
-        cursor.execute("USE DATABASE daily_sales")
-        cursor.execute(f"create table IF NOT EXISTS sales_table_after_{last_date}")
+        cursor.execute("USE DATABASE SALES_ANALYTICS_DWH")
+        cursor.execute("USE SCHEMA SALES_ANALYTICS_DWH.SALES")
+        cursor.execute("""create table IF NOT EXISTS sales_table(sale_id INT NOT NULL, \
+                       date_time TIMESTAMP NOT NULL, cust_id INT NOT NULL, product_id INT NOT NULL, \
+                       quantity INT NOT NULL, unit_price INT NOT NULL, total_price INT NOT NULL)""")
 
-        status, nchunks, nrows,_ = write_pandas(connection,df,f"sales_table_after_{last_date}") # _ is the output of COPY cmd being executed in database
+        status, nchunks, nrows,_ = write_pandas(connection, df, "sales_table", quote_identifiers=False) # _ is the output of COPY cmd being executed in database
         print(f"Status of the function: {status}\n number of rows inserted: {nrows}")
     finally:
         connection.close()
